5acd17e73b1b52518468c037230f4e9f17d5f919
tamimNumrah <tamim@numrah.com>
2023-04-10 01:27:34 +1000

    Main activity designed

--- app/src/main/java/com/tamim/task31c/MainActivity.java
+++ app/src/main/java/com/tamim/task31c/MainActivity.java
@@ -2,13 +2,38 @@ package com.tamim.task31c;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import com.google.android.material.textfield.TextInputLayout;
 import android.os.Bundle;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button startButton;
+    TextInputLayout nameTextInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+        nameTextInput = findViewById(R.id.nameInputLayout);
+    }
+
+    public void onStartButtonPressed(View view) {
+        System.out.println("Start Button Pressed!");
+        String name = nameTextInput.getEditText().getText().toString();
+        if (name.matches("")) {
+            showToast(getString(R.string.name_empty_error));
+            return;
+        }
+        Intent quizIntent = new Intent(MainActivity.this, QuizActivity.class);
+        MainActivity.this.startActivity(quizIntent);
+    }
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
